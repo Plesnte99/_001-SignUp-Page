cbfbc39ea55a540ed03d8f910f3e03bf5973b966
Plesnte99 <sharjeel.work9@gmail.com>
2023-10-14 16:14:53 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`System is running on port ${port}!`);
+});
